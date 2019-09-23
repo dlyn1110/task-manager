f0e187aae8bef0be395f58674efcefd910d96aef
Dani Lyn <dlyn1110@gmail.com>
2019-09-23 23:29:50 +0000

    added login and signup

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,12 @@
 class TasksController < ApplicationController
 
-
+  get '/tasks'do
+      if logged_in?
+        @tasks = current_user.tasks
+        erb :'tasks/index'
+      else
+        redirect to '/signup'
+     end
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,25 @@ class UsersController < ApplicationController
     erb :'users/login'
   end
 
+  post "/login" do
+   if params[:email] == "" || params[:password] == ""
+     flash[:message] = "Please ensure all the fields are populated."
+     erb :"/users/login"
+   else
+   user = User.find_by(:email => params[:email])
+     if !user
+       flash[:message] = "You do not have an account."
+       erb :"users/create_user"
+     else
+       if user && user.authenticate(params[:password])
+         session[:user_id] = user.id
+         redirect to "/tasks"
+       else
+         flash[:message] = "Your password is incorrect."
+         erb :"/users/login"
+       end
+     end
+   end
+ end
+
 end
